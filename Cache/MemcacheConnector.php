cc65ce4ca5cde8883ef9ba1978dbb8c2df91a244
ander.yang <yyangtze@gmail.com>
2015-05-04 22:15:53 +0800

    memcache
    Signed-off-by: ander.yang <yyangtze@gmail.com>

---
+++ Cache/MemcacheConnector.php
@@ -0,0 +1,50 @@
+<?php 
+/**
+ * Clone from laravel
+ */
+
+namespace Cache;
+
+use Memcached;
+use RuntimeException;
+
+class MemcacheConnector {
+
+	/**
+	 * Create a new Memcached connection.
+	 *
+	 * @param  array  $servers
+	 * @return \Memcached
+	 *
+	 * @throws \RuntimeException
+	 */
+	public static function connect(array $servers)
+	{
+		
+		$memcached = new Memcached;
+
+		// For each server in the array, we'll just extract the configuration and add
+		// the server to the Memcached connection. Once we have added all of these
+		// servers we'll verify the connection is successful and return it back.
+		foreach ($servers as $server)
+		{
+			$memcached->addServer(
+				$server['host'], $server['port'], $server['weight']
+			);
+		}
+
+		$memcachedStatus = $memcached->getVersion();
+
+		if ( ! is_array($memcachedStatus))
+		{
+			throw new RuntimeException("No Memcached servers added.");
+		}
+
+		if (in_array('255.255.255', $memcachedStatus) && count(array_unique($memcachedStatus)) === 1)
+		{
+			throw new RuntimeException("Could not establish Memcached connection.");
+		}
+
+		return $memcached;
+	}
+}
